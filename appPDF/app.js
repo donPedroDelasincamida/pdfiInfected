25ecca6ab64d4390fed8c3bde7ccb32fb4b91b2b
Lucien Casteres <lucien.casteres@enseirb-matmeca.fr>
2020-01-07 16:33:35 +0100

    small changes

--- appPDF/app.js
+++ appPDF/app.js
@@ -1,24 +1,32 @@
 const { spawn } = require('child_process');
 const exec = require('child_process').exec;
+var shell = require('shelljs');
 
+shell.config.execPath = "/usr/local/bin/node"
 
 // Event Listener on the connect button 
 document.getElementById("connect").addEventListener("click", function () {
     const ls = exec('sh test.sh');
     var resDiv = document.getElementById("result");
-    var final="ls : ";
-    ls.stdout.on('data', (data) => {
-        console.log(data);
-        if (data.indexOf("ls :") != -1)
-        {
-            resDiv.innerHTML="Loading";
-        }
-        else{
-            final += data
-            resDiv.innerHTML = final;
-            changeToSuccess(this,"Connected");
-        }
-    });  
+    resDiv.innerHTML = shell.execAsync();
+    shell.cd();
+    console.log(shell.ls().stdout);
+
+    // var final="ls : ";
+    // ls.stdout.on('data', (data) => {
+    //     console.log(data);
+    //     if (data.indexOf("ls :") != -1)
+    //     {
+    //         resDiv.innerHTML="Loading";
+    //     }
+    //     else{
+    //         final += data
+    //         resDiv.innerHTML = final;
+    //         changeToSuccess(this,"Connected");
+    //     }
+    // });  
+
+
 })
 
 function changeToSuccess(node, text){
